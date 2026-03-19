b4bb45f3e2c3e275b661658af0c25a7da300f34d
Félice <felice@tsf-516-wpa-3-126.epfl.ch>
2026-03-19 15:36:22 +0100

    add solve ODE for xi

---
+++ src/utils/solve_Xi_eq.m
@@ -0,0 +1,43 @@
+function [Xi_fine, Xip_fine] = solve_Xi_eq(r_fine, Xibc_left, Xibc_right, qfun, qpfun)
+% Solve the ODE:
+%   d/dr [r^3*(2mu-1)^2 * dXi/dr] - 3*r*(2mu-1)^2 * Xi = 0
+% where mu(r) = 1/q(r)
+%
+% INPUTS:
+%   r_fine      : vector of r points for evaluation
+%   Xibc_left   : BC value at left endpoint (r_fine(1))
+%   Xibc_right  : BC value at right endpoint (r_fine(end))
+%   qfun        : function handle q(r)
+%   qpfun       : function handle dq/dr
+%
+% OUTPUTS:
+%   Xi_fine     : solution xi(r) on r_fine
+%   Xip_fine    : derivative dxi/dr on r_fine
+
+% mu and its derivative
+mufun  = @(r)  1 ./ qfun(r);
+mupfun = @(r) -qpfun(r) ./ qfun(r).^2;
+
+% Coefficient w(r) = r^3*(2mu-1)^2 and its log-derivative w'/w
+% w'/w = 3/r + 2*2*mu'/(2*mu-1)
+log_wp = @(r) 3/r + 4*mupfun(r) / (2*mufun(r) - 1);
+
+% ODE as first-order system: Y = [Xi; Xi']
+% Xi'' = -(w'/w)*Xi' + 3/r^2 * Xi   [since 3r(2mu-1)^2/w = 3/r^2]
+odefun = @(r, Y) [ Y(2); -log_wp(r) * Y(2) + (3 / r^2) * Y(1) ];
+
+% Boundary conditions: Xi(r_left) = Xibc_left, Xi(r_right) = Xibc_right
+bcfun = @(Ya, Yb) [ Ya(1) - Xibc_left; Yb(1) - Xibc_right ];
+
+% Initial guess: linear interpolation between BCs
+solinit = bvpinit(linspace(r_fine(1), r_fine(end), 10), @(r) [Xibc_right * r; Xibc_right]);
+
+% Solve
+options = bvpset('RelTol', 1e-8, 'AbsTol', 1e-10);
+sol = bvp4c(odefun, bcfun, solinit, options);
+
+% Evaluate on fine grid
+Xi_fine  = deval(sol, r_fine, 1);
+Xip_fine = deval(sol, r_fine, 2);
+
+end
