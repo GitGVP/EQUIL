03d59ee43a9212f9235462dd5ba9d7cf1966a082
Guillaume Van Parys <guillaume.vanparys@epfl.ch>
2026-04-22 13:41:03 +0200

    upper sidebands

--- src/utils/solve_Xi_eq.m
+++ src/utils/solve_Xi_eq.m
@@ -1,4 +1,4 @@
-function [Xi_fine, Xip_fine] = solve_Xi_eq(r_fine, Xibc_left, Xibc_right, qfun, qpfun, V, neumann_out)
+function [Xi_fine, Xip_fine] = solve_Xi_eq(r_fine, Xibc_left, Xibc_right, qfun, qpfun, ml, neumann_out, V)
 % Solve the ODE:
 %   d/dr [r^3*(2mu-1)^2 * dXi/dr] - 3*r*(2mu-1)^2 * Xi = 0
 % where mu(r) = 1/q(r)
@@ -13,28 +13,22 @@ function [Xi_fine, Xip_fine] = solve_Xi_eq(r_fine, Xibc_left, Xibc_right, qfun, 
 % OUTPUTS:
 %   Xi_fine     : solution xi(r) on r_fine
 %   Xip_fine    : derivative dxi/dr on r_fine
+if  nargin < 8
+    V = @(r) 0; 
+end
 if  nargin < 7
     neumann_out = false;
 end
 if  nargin < 6
-    V = @(r) 0; 
-    neumann_out = false;
+    ml=2;
 end
-% mu and its derivative
-mufun  = @(r)  1 ./ qfun(r);
-mupfun = @(r) -qpfun(r) ./ qfun(r).^2;
-ml=2;
+
+
 d2coeff = @(r) (-1 + ml./qfun(r)).^2.*r.^2;
 d1coeff = @(r) -(((ml - qfun(r)).*r.*(-3.*ml.*qfun(r) + ...
 3.*qfun(r).^2 + 2.*ml.*qpfun(r).*r))./qfun(r).^3);
 d0coeff = @(r) (1 - ml.^2).*(-1 + ml./qfun(r)).^2;
-% Coefficient w(r) = r^3*(2mu-1)^2 and its log-derivative w'/w
-% w'/w = 3/r + 2*2*mu'/(2*mu-1)
-log_wp = @(r) 3/r + 4*mupfun(r) / (2*mufun(r) - 1);
 
-% ODE as first-order system: Y = [Xi; Xi']
-% Xi'' = -(w'/w)*Xi' + 3/r^2 * Xi   [since 3r(2mu-1)^2/w = 3/r^2]
-%odefun = @(r, Y) [ Y(2); -log_wp(r) * Y(2) + (3 / r^2) * Y(1) + V(r)];
 odefun = @(r, Y) [ Y(2); -(d1coeff(r) * Y(2) + d0coeff(r) * Y(1) + V(r)) ./ d2coeff(r)];
 % Boundary conditions: Xi(r_left) = Xibc_left, Xi(r_right) = Xibc_right
 if neumann_out
